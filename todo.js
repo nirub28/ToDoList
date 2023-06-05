1aa66445938b2f6cacea9a877a0e605683824670
Niranjan P B <niranjanbgowda123@gmail.com>
2023-06-05 23:42:56 +0530

    Delete and Complete task

--- todo.js
+++ todo.js
@@ -3,40 +3,58 @@ const taskList = document.getElementById("list");
 const addTaskInput = document.getElementById("add");
 const tasksCounter = document.getElementById("tasks-counter");
 
-console.log("Working");
+function addTasksToList(task) {
+  const li = document.createElement("li");
 
-function renderList() {}
+  li.innerHTML = `
+           <input type="checkbox" id="${task.id}" ${
+    task.done ? "checked" : ""
+  } class="custom-checkbox">
+           <label for="${task.id}">${task.input}</label>
+           <img src="https://cdn-icons-png.flaticon.com/256/1617/1617543.png" class="delete" data-id="${
+             task.id
+           }" />
+   `;
+  taskList.append(li);
+}
+
+function renderList() {
+  taskList.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    addTasksToList(tasks[i]);
+  }
+  tasksCounter.innerHTML = tasks.length;
+}
 
 function markTaskAsComplete(taskId) {
-    const task = tasks.filter(task => {
-        return task.id === taskId;  
-    });
+  const task = tasks.filter((task) => {
+    return task.id == taskId;
+  });
 
-    if(task>0){
-            const completeTask = task[0];
-            completeTask.done=!completeTask.done; // to toggle check box to mark complet and not
-            renderList();
-            return;
-    }
+  if (task.length > 0) {
+    const completeTask = task[0];
+    completeTask.done = !completeTask.done; // to toggle check box to mark complet and not
+    renderList();
+    return;
+  }
 }
 
 function deleteTask(taskId) {
-    const newTasks=tasks.filter(task => {
-        return task.id !== taskId;  // it will remove the task with taskId matches from array
-    });
+  const newTasks = tasks.filter((task) => {
+    return task.id !== taskId; // it will remove the task with taskId matches from array
+  });
 
-    tasks:newTasks; // as we render tasks array, we need latest array after delting task
-    renderList();
+  tasks= newTasks; // as we render tasks array, we need latest array after delting task
+  renderList();
 }
 
-
 function addTask(task) {
-   if(task){
+  if (task) {
     tasks.push(task);
     renderList();
     return;
-   }
-   
+  }
 }
 
 function showNotification(text) {
@@ -44,7 +62,8 @@ function showNotification(text) {
 }
 
 function handleInputKeyPress(e) {
-  if (e.key === "Enter") {      // when enter key pressed trigger this
+  if (e.key === "Enter") {
+    // when enter key pressed trigger this
 
     const input = e.target.value;
 
@@ -56,12 +75,27 @@ function handleInputKeyPress(e) {
     const task = {
       input,
       id: Date.now().toString(), // as we need reference to delete a task
-      done: false,               // to store if task is complted or not
+      done: false, // to store if task is complted or not
     };
 
-    e.target.value = "";       // to clear a input text box, once submitted
+    e.target.value = ""; // to clear a input text box, once submitted
     addTask(task);
   }
 }
 
-addTaskInput.addEventListener('keyup',handleInputKeyPress);
+function handleClickEvent(e) {
+  const target = e.target;
+
+  if (target.className === "delete") {
+    const taskId = target.dataset.id;
+    deleteTask(taskId);
+    return;
+  } else if (target.className === "custom-checkbox") {
+    const taskId = target.id;
+    markTaskAsComplete(taskId);
+    return;
+  }
+}
+
+addTaskInput.addEventListener("keyup", handleInputKeyPress);
+document.addEventListener("click", handleClickEvent);
